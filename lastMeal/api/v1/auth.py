ee714f04aafd874f8bc3a1b23986e2d74494ac6f
petek222 <peter.e.koncelik@vanderbilt.edu>
2021-02-28 01:34:29 -0600

    registration endpoint with json responses

--- lastMeal/api/v1/auth.py
+++ lastMeal/api/v1/auth.py
@@ -15,40 +15,56 @@ db = mongo_client["user"] # Added line to have access to db
 host_info = mongo_client['HOST'] 
 
 # Create/Register a new user
-# We want to replace the returns and params with JSON grabs and responses
+# Syntax can probably be cleaned up: look at docs
 # This also needs more robust error handling via try-catch
 @bp.route('', methods=['GET'])
 def register_user():
-    print ("\nhost:", host_info)
     print("let's try to register a new user!")
+
+    request_data = request.json
+    validation = User()
 
     # Obviously these fields will need to take in the JSON sent from the client in actuality
     # This is just for testing
-    username = 'tedk2'
-    email = 'teddy.koncelik@loyola.edu'
+    username = request_data['username']
+    email = request_data['email']
+    password = request_data['password']
     user_salt = bcrypt.gensalt()
-    password = "userpassword"
     user_hash = bcrypt.hashpw(password.encode('utf-8'), user_salt) # Make sure this accepts API argument when real
-
-    validation = User()
     
     # First check if a user with the given username/email already exists
     if db.user.find({'username': { "$in": [username]}}).count() > 0 or db.user.find({'email': { "$in": [email]}}).count() > 0:
-        return "<h1>Username/Email already exists: please try different values</h1>" 
+        response_obj = {
+            "error": "supplied username already exists"
+        }
+        resp = make_response(response_obj, 400)
+        resp.headers['Content-Type'] = 'application/json'
+        return resp
 
     # Perform Validation Checks on requisite fields, and create the user if valid
     if validation.validate_username(username) and validation.validate_email(email):
         # Using mongoengine/pymongo schema
         my_user = User(username = username, email=email, first_name='pedro', last_name='babon', salt=user_salt, hash=user_hash).save()
-        return "<h1>User Successfully Registered!"
+        response_obj = {
+            "username": username,
+            "email": email
+        }
+        resp = make_response(response_obj, 201)
+        resp.headers['Content-Type'] = 'application/json'
+        return resp
 
     # Otherwise return a false (change for API as needed)
     else:
-        return "<h1>Invalid Username/Email, please try again!</h1>"
+        response_obj = {
+            "error": "Invalid username/password"
+        }
+        resp = make_response(response_obj, 400)
+        resp.headers['Content-Type'] = 'application/json'
+        return resp
 
 # Log In as an existing user
 # Currently sessions are omitted/being punted to the native client
-# We want to replace the returns and params with JSON grabs and responses
+# Syntax can probably be cleaned up: look at docs
 # This also needs more robust error handling via try-catch
 @bp.route('/login', methods=['GET', 'POST'])
 def login_user():
@@ -63,7 +79,12 @@ def login_user():
 
     # First check that the given username even exists in the db
     if not db.user.find({'username': { "$in": [username]}}).count() > 0:
-        return "<h1>Supplied username does not exist</h1>" 
+        response_obj = {
+            "error": "supplied username does not exist"
+        }
+        resp = make_response(response_obj, 400)
+        resp.headers['Content-Type'] = 'application/json'
+        return resp
     
     # If the user exists, we grab the stored salt and hash from the db
     user_data = db.user.find_one({'username': username}, {'hash':1, 'salt':1, 'email':1})
