088ba7af571361dcd81aac5bf504bba4ca0a3745
petek222 <peter.e.koncelik@vanderbilt.edu>
2021-03-02 01:20:03 -0600

    added user profile fetch endpoints

--- lastMeal/api/v1/auth.py
+++ lastMeal/api/v1/auth.py
@@ -10,10 +10,7 @@ from bson.objectid import ObjectId
 bp = Blueprint('auth', __name__, url_prefix='/v1/user')
 
 # Create/Register a new user
-# Syntax can probably be cleaned up: look at docs
-# This also needs more robust error handling via try-catch
 # ******************************************************************************
-# Log In as an existing user
 #
 # @param request_data['username'] username for account
 # @param request_data['password'] password for account
@@ -69,11 +66,7 @@ def register_user():
 
 # Log In as an existing user
 # Currently sessions are omitted/being punted to the native client
-# Syntax can probably be cleaned up: look at docs
-# This also needs more robust error handling via try-catch
-#
 # ******************************************************************************
-# Log In as an existing user
 #
 # @param request_data['username'] username for login
 # @param request_data['password'] password for login
@@ -100,6 +93,8 @@ def login_user():
     # If the user exists, we grab it from the db
     user_data = User.objects(username=username)[0]
 
+    # ** Add a try-catch here to deal with any auth problems **
+
     # Impl doesnt use salt due to builtin
     if user_data.authenticate_user(password):
         response_obj = {
@@ -118,24 +113,100 @@ def login_user():
         return resp
 
 # Update preexising user profile
+# Let a user update their username, password, email, first name, lastname
+# ******************************************************************************
+#
+# @param request_data['current_username'] username currently tied to account
+# @param request_data['username'] username for login
+# @param request_data['password'] password for login
 @bp.route('/update', methods=['PUT'])
 def update_user():
-    return "<h1>Here is where we can have the user update their profile/login information!</h1>"
+    print("Update preexisting user")
+
+    # grab data from request body and instiantiate validator
+    request_data = request.json
+    current_username = request_data['current_username']
+    validation = User()
+
+    # Using mongoengine/pymongo schema to update and save the user's information
+    # my_user = User(username = username, email=email, first_name=firstName, last_name=lastName, pantry=pantry_id)
+    try:
+        # my_user.set_password(password)
+        # my_user.validate()
+        # my_user.save()
+        user_query = {"username": current_username}
+        # user_update_obj = {'$set': request_data}
+
+        validation = User()
+
+        print("TEST")
+        # print(User.objects(username=current_username))
+        print(current_username)
+
+        User.objects(username=current_username)
+        
+        # data.update(username=request_data['username'])
+
+
+        response_obj = {
+            "username": "HIIII",
+            "email": "HEEEEE"
+        }
+        resp = make_response(response_obj, 201)
+        resp.headers['Content-Type'] = 'application/json'
+        return resp
+    except Exception as e:
+        print(e)
+        response_obj = {
+            "error": "Invalid username/password"
+        }
+        resp = make_response(response_obj, 400)
+        resp.headers['Content-Type'] = 'application/json'
+        return resp
+
+
 
 # Log out as an existing user
 @bp.route('/logout', methods=['GET'])
 def logout_user():
-    return "<h1>Here is where we can have the user log out!</h1>"
+    return "Placeholder: this will most likely be handled by the frontend"
 
 # ********************************************************************************
 # Basic Getters/Setters for Users below
 
 # Fetch user profile data by username (other?)
-@bp.route('/:username', methods=['GET'])
-def fetch_user():
-    return "<h1>Here is where we can fetch the user's information!</h1>"
+@bp.route('/<username>', methods=['GET'])
+def fetch_user(username):
+
+    # First check that the given username even exists in the db
+    if not User.objects(username=username):
+        response_obj = {
+            "error": "supplied username does not exist"
+        }
+        resp = make_response(response_obj, 400)
+        resp.headers['Content-Type'] = 'application/json'
+        return resp
+
+    # If the user exists, we grab it from the db and return it
+    user_data = User.objects.get(username=username).to_json()
+    response_obj = user_data
+    resp = make_response(response_obj, 200)
+    resp.headers['Content-Type'] = 'application/json'
+    return resp
+    
 
 # Check if a profile exists with the given username
-@bp.route('/:username', methods=['HEAD'])
-def check_user():
-    return "<h1>Here is where we can check if a user exists!</h1>"
+@bp.route('/<username>', methods=['HEAD'])
+def check_user(username):
+
+    # If the user does not exist, send back a 404 not found
+    if not User.objects(username=username):
+        resp = make_response(404)
+        return resp
+
+    # If the user exists, we grab it from the db and return it
+    user_data = User.objects.get(username=username).to_json()
+    resp = make_response(200)
+    return resp
+    
+
