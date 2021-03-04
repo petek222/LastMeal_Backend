4a823c45f48a493d55537a2f9affb65c0c9a3b81
John Mathena <jmmathena@gmail.com>
2021-03-04 19:54:21 +0000

    Clean up auth.py and write create_ingredient

--- lastMeal/api/v1/pantry.py
+++ lastMeal/api/v1/pantry.py
@@ -1,34 +1,51 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for, make_response
+    Blueprint, flash, g, redirect, render_template, request, session, url_for, make_response, Response
 )
 import json
+from datetime import datetime
 from lastMeal.models.user import User
+from lastMeal.models.ingredient import Ingredient
 from bson.objectid import ObjectId
 
 # Blueprint for connection to main process
 bp = Blueprint('pantry', __name__, url_prefix='/v1/pantry')
-
 
 # Create a new ingredient and associate it with a user
 # ******************************************************************************
 @bp.route('/create/<username>', methods=["POST"])
 def create_ingredient(username):
     print("Creating a new ingredient.")
+    request_data = request.json
+    name = request_data["name"]
+    quantity = request_data["quantity"]
+    exp_date = request_data["expiration_date"]
+    exp_date = datetime.strptime(exp_date, "%Y-%m-%d")
+    user = User.objects(username=username)
+    if user.first() == None:
+        return ({"error": "requested user not found"}, 400)
 
-# Retrieve all ingredients associated with a user
-# ******************************************************************************
-@bp.route('/<username>', methods=["GET"])
-def create_ingredient(username):
-    print("Retrieving ingredients.")
+    try:
+        my_ingredient = Ingredient(name=name, quantity=quantity, expiration_date=exp_date, user=user.first())
+        my_ingredient.save()
+        return ({"name": name, "quantity": quantity, "expiration_date": exp_date}, 201)
+    except Exception as e:
+        print(e)
+        return ({"error": "could not save requested ingredient"}, 401)
 
-# Update an ingredient based on the ingredient ID
-# ******************************************************************************
-@bp.route('/update/<ingredient_id>', methods=["POST"])
-def create_ingredient(ingredient_id):
-    print("Updating ingredient.")
+## Retrieve all ingredients associated with a user
+## ******************************************************************************
+#@bp.route('/<username>', methods=["GET"])
+#def create_ingredient(username):
+    #print("Retrieving ingredients.")
 
-# Delete an ingredient based on the ingredient ID
-# ******************************************************************************
-@bp.route('/delete/<ingredient_id>', methods=["DELETE"])
-def create_ingredient(ingredient_id):
-    print("Deleting ingredient.")
+## Update an ingredient based on the ingredient ID
+## ******************************************************************************
+#@bp.route('/update/<ingredient_id>', methods=["POST"])
+#def create_ingredient(ingredient_id):
+    #print("Updating ingredient.")
+
+## Delete an ingredient based on the ingredient ID
+## ******************************************************************************
+#@bp.route('/delete/<ingredient_id>', methods=["DELETE"])
+#def create_ingredient(ingredient_id):
+    #print("Deleting ingredient.")
--- lastMeal/models/ingredient.py
+++ lastMeal/models/ingredient.py
@@ -1,11 +1,12 @@
 from mongoengine import *
 from mongoengine.errors import ValidationError
+from lastMeal.models.user import User
 
 connect('lastMeal')
 
 class Ingredient(Document):
     name = StringField(max_length=50)
-    expiration_date = DateTimeField(required=True)
+    expiration_date = DateTimeField()
     quantity = DecimalField(required=True)
     #units = StringField(required=True)
-    user = ObjectIdField(db_field='user')
+    user = ReferenceField(User, required=True)
