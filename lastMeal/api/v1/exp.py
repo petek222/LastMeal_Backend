d8c3b249296d956e5e58fe4414f66a9aa7ff18cf
petek222 <peter.e.koncelik@vanderbilt.edu>
2021-04-03 17:57:07 -0500

    expiration API skeleton + date parser

---
+++ lastMeal/api/v1/exp.py
@@ -0,0 +1,94 @@
+# API for pulling and computing suggested expiration dates
+
+# This is how dates are being sent from the frontend to the server
+# 2021-04-30
+# string
+
+# This is how dates are being recieved from the server by the frontend (we think)
+# Object {
+#   "$date": 1619481600000,
+# }
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for, make_response, Response
+)
+import json
+from datetime import datetime
+from lastMeal.models.user import User
+from lastMeal.models.ingredient import Ingredient
+from bson.objectid import ObjectId
+
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+import datetime as dt
+
+# Blueprint for connection to main process
+bp = Blueprint('exp', __name__, url_prefix='/v1/exp')
+
+# Request Format:
+# /v1/photos?ingredient=apple redirects us to /v1/photos/?ingredient=apple
+@bp.route('', methods=['GET'])
+@jwt_required()
+def fetch_expiration():
+    ingredient = request.args.get("ingredient")
+
+    if ingredient is None:
+        return ({"error": "no ingredient was passed"}, 400)
+
+    try:
+        pass # Make request to expiration database
+    
+    except Exception as e:
+        print(e)
+        return ({"error": "Image could not be retrieved"}, 400)
+
+
+# We want to parse the date strings and return them in the format needed
+# by the frontend for parsing
+# 
+# IMP NOTE: I am opting for a conservative-estimate approach to the expiration
+# data dataset. That is, If a given date supplied a range, I will be selecting
+# and parsing based off the minimum value from that range (ie. 2-4 weeks => 2 weeks)
+#
+# Input Data Formats:
+# 2 days/2 weeks/2 months/2 years
+# 2-4 days/2-4 weeks/2-4 months/2-4 years
+#
+# Output Data Formats (calculated from current Date)
+# 2021-04-30 (yyyy-mm-dd)
+# 1619481600000 (seconds since epoch)
+def parse_dates(date_string):
+
+    # Parse the string into day and time
+    date_parsed = date_string.split(' ')
+
+    number_string = date_parsed[0]
+    duration = date_parsed[1]
+    if '-' in number_string:
+        number_string = number_string[0]
+    
+    number = int(number_string)
+
+    # Convert the duration to value using dict 
+    expiration = None
+    today = dt.date.today()
+
+    if duration == 'days':
+        expiration = today + dt.timedelta(days=number)
+    elif duration == 'weeks':
+        expiration = today + dt.timedelta(weeks=number)
+    elif duration == 'months':
+        week_count = 4 * number
+        expiration = today + dt.timedelta(weeks=week_count)
+    elif duration == 'years':
+        week_count = 52 * number
+        expiration = today + dt.timedelta(weeks=week_count)
+    else:
+        expiration = "Error; no value able to be computed"
+    
+
+    print("Value after parsing:")
+    print(expiration)
+
+    # Value returned as: 2023-04-01
+    return expiration
